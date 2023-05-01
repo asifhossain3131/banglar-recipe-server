4cb362a90546a5504e1d4626d1e55cd624a0162f
asifhossain3131 <asifhossain3131@gmail.com>
2023-05-02 00:02:15 +0600

    fake data added

--- index.js
+++ index.js
@@ -3,11 +3,37 @@ const app=express()
 
 const port=process.env.PORT||5000;
 
+const chefs=require('./data/chefs.json')
+const foods=require('./data/foods.json')
+const newses=require('./data/latestNews.json')
+
 
 app.get('/', (req,res)=>{
     res.send('chef server is running')
 })
 
+app.get('/chefs', (req, res)=>{
+    res.send(chefs)
+})
 
+app.get('/chefRecipes/:id', (req, res)=>{
+const id=req.params.id
+const target=chefs.find(chef=>chef.chefId===id)
+res.send(target)
+})
+
+app.get('/foods', (req,res)=>{
+    res.send(foods)
+})
+
+app.get('/food/:id', (req, res)=>{
+    const id=req.params.id 
+    const target=foods.find(food=>food.categoryId===id)
+    res.send(target)
+})
+
+app.get('/news', (req, res)=>{
+    res.send(newses)
+})
 
 app.listen(port)
